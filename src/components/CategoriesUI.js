cb3186f186149335a825cfdba9203955d7531e89
Parastoo Moghaddam <parastoomoghaddam1997@gmail.com>
2019-07-23 19:00:00 +0430

    App UI almost done.

---
+++ src/components/CategoriesUI.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CategoriesUI = () => (
+  <div>
+    <ul>
+      <li><a className="active" href="#category1" >category #1</a></li>
+      <li><a href="#category2" >category #2</a></li>
+      <li><a href="#category3" >category #3</a></li>
+      <li><a href="#category4" >category #4</a></li>
+      <li><a href="#category5" >category #5</a></li>
+    </ul>
+  </div>
+);
+
+export default CategoriesUI;
---
+++ src/components/ImagesUI.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImagesUI = () => (
+  <div className="div-width">
+    <h1>images</h1>
+  </div>
+);
+
+export default ImagesUI;
